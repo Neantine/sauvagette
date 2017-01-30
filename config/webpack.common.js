d24c4301a11d26ad510309deb93bf8d119bab3d0
Neantine <4sth4rtee@gmail.com>
2017-01-30 11:47:29 +0100

    test assets path

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,7 +25,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'Lilith Néantine',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
